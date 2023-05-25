1fb9e031e9496ebe3c57d8072c3fa4c66841fc0a
Kitwana Akil <kakil@me.com>
2023-05-25 13:30:44 -0400

    Initial project commit.  Updated minimum iOS version for app.  Initial UI changes.

--- Pomodoro Timer/ContentView.swift
+++ Pomodoro Timer/ContentView.swift
@@ -10,12 +10,11 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
             Text("Hello, world!")
         }
         .padding()
+        .font(.system(size: 80))
+        .fontWeight(.bold)
     }
 }
 
