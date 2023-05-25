5b9bd787b6c9963a414aa03d0e5e1547ddc47f19
Kitwana Akil <kakil@me.com>
2023-05-25 13:25:55 -0400

    Initial Commit

---
+++ Pomodoro Timer/Pomodoro_TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pomodoro_TimerApp.swift
+//  Pomodoro Timer
+//
+//  Created by Kitwana Akil on 5/25/23.
+//
+
+import SwiftUI
+
+@main
+struct Pomodoro_TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
