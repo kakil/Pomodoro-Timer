39a68e7b34b75844fbf09036558e4bb37f659b73
Kitwana Akil <kakil@me.com>
2023-05-25 21:27:10 -0400

    Updated timer to add circular slider and add colors.

--- Pomodoro Timer/ContentView.swift
+++ Pomodoro Timer/ContentView.swift
@@ -18,6 +18,16 @@ struct ContentView: View {
     let workPeriodLength = 1500
     let breakPeriodLength = 300
     
+    let workTimeBackgroundColor = Color(red: 51/255, green: 59/255, blue: 78/255)
+    let breakTimeBackgroundColor = Color(red: 133/255, green: 143/255, blue: 143/255, opacity: 1.0)
+    //let breakTimeBackgroundColor = Color(red: 48/255, green: 57/255, blue: 75/255, opacity: 0.5)
+    let lightGreyBackgroundColor = Color(red: 227/255, green: 225/255, blue: 222/255, opacity: 1.0)
+    let lightGreyColor = Color(red: 227/255, green: 225/255, blue: 222/255, opacity: 1.0)
+    let greenColor = Color(red: 7/255, green: 233/255, blue: 139/255, opacity: 1.0)
+    let greyColor = Color(red: 139/255, green: 153/255, blue: 145/255, opacity: 1.0)
+    let redColor = Color(red: 245/255, green: 81/255, blue: 81/255, opacity: 1.0)
+    let blueGrey = Color(red: 148/255, green: 186/255, blue: 198/255, opacity: 1.0)
+    
     
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     
@@ -30,15 +40,25 @@ struct ContentView: View {
     }
     
     var backgroundColor: Color {
-        return timeToWork ? Color.black : Color.white
+        return workTimeBackgroundColor
     }
     
     var textColor: Color {
-        return timeToWork ? Color.white : Color.black
+        return lightGreyColor
     }
     
     var timerText: String {
         return timeToWork ? "Time To Work!" : "Break Time!"
+    }
+    
+    var sliderProgress: Double {
+        let totalTime = timeToWork ? workPeriodLength : breakPeriodLength
+        let timeRemaining = timeToWork ? workTimeRemaining : breakTimeRemaining
+        return Double(timeRemaining) / Double(totalTime) * 360.0
+    }
+    
+    var sliderStrokeColor: Color {
+        return timeToWork ? redColor : greenColor
     }
     
     var body: some View {
@@ -47,26 +67,46 @@ struct ContentView: View {
             VStack (spacing: 30){
                 Text("Pomodoro Timer")
                     .font(.system(size: 90))
+                    .foregroundColor(textColor)
                 Text("By AkilDev LLC")
                     .font(.system(size: 45))
-                Text(String(convertSecondsToTime(seconds: timeToShow)))
-                    .font(.system(size: 80))
+                
+                ZStack {
+                    Circle()
+                        .trim(from: 0.0, to: 1.0)
+                        .stroke(greyColor, lineWidth: 10)
+                        .frame(width: 300, height: 300)
+                    
+                    Circle()
+                        .trim(from: 0.0, to: CGFloat(sliderProgress) / 360.0)
+                        .stroke(sliderStrokeColor, lineWidth: 20)
+                        .frame(width: 300, height: 300)
+                        .rotationEffect(.degrees(-90))
+                    Text(String(convertSecondsToTime(seconds: timeToShow)))
+                        .font(.system(size: 80))
+//                    Text("\(Int(sliderProgress))")
+//                        .font(.system(size: 40))
+//                        .foregroundColor(.white)
+                            
+                }
+               
                 Text(timerText)
                     .font(.system(size: 60))
                     .padding()
+                
                 ZStack {
                     Circle()
-                        .foregroundColor(Color.blue)
+                        .foregroundColor(greenColor)
                         .frame(width: 120, height: 120)
                     Image(systemName: isPaused ? "play.fill" : "pause.fill" )
                         .font(.system(size: 60))
                         .fontWeight(.bold)
-                        .foregroundColor(.white)
-                            
+                        .foregroundColor(lightGreyColor)
                 }
                 .onTapGesture {
                     isPaused.toggle()
                 }
+                
                     
             }
             .padding()
@@ -106,3 +146,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
